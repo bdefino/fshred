66aeb2459da8e96d7df0e5f818209f7a0b131e31
bdefino <bailey.defino@stonybrook.edu>
2020-02-06 20:08:36 -0500

    license

--- fshred-tiny.c
+++ fshred-tiny.c
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2020 Bailey Defino
+<https://bdefino.github.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <errno.h>
 #include <fcntl.h>
 #include <ftw.h>
@@ -238,7 +255,7 @@ static void usage(const char *executable);
 /* print the usage and option information */
 static void help(const char *executable) {
   usage(executable != NULL ? executable : "?");
-  puts(OPTIONS);
+  fprintf(stderr, OPTIONS);
 }
 
 int main(int argc, char **argv) {
